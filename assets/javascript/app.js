80af916a016b37ad4cb636001d6c7f01def16c6c
tleytek <lucidmat94@gmail.com>
2018-07-12 15:16:51 -0400

    Functionality Complete, Presentation lacking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,67 @@
 $(document).ready(function() {
-  var time = 40;
-  var correctAnswers;
-  var incorrectAnswers;
-  var unAnswered;
+  var time = 70;
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
+  var unAnswered = 0;
   var clockRunning = false;
-  var intervalID;
+  var intervalId;
+
+  //Trying to think of a better way to store questions and their answers.
+
   // making an id for the button called 'StartButton' did not work for some reason, but having just 'button' works??????
-  $("button").on("click", function() {
-    $("#start").hide();
+  $("#startButton").on("click", function() {
+    $("#startButton").hide();
     $("#main").show();
-    run();
-    clockRunning = true;
+    initializeTimer();
   });
 
-  if (time === 0) {
+  function initializeTimer() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  function score() {
+    //Go through each radio group
+    for (var i = 1; i <= 7; i++) {
+      //variable to see if no radio buttons were checked
+      var unchecked = true;
+
+      //This line confused me alot because it wasn't clear to me at first that when you assign an HTML element by name to a variable, it will apply every element with that name into your variable, thus becoming an array, I think....
+      var question = document.getElementsByName("question" + i);
+
+      //Iterates through the current choices array and looks to see whether a correct or incorrect answer was chosen, and changes the boolean unchecked value to false.
+      for (var j = 0; j < question.length; j++) {
+        answers = question[j];
+        if (answers.value === "correct" && answers.checked) {
+          correctAnswers++;
+          unchecked = false;
+        } else if (answers.value === "incorrect" && answers.checked) {
+          incorrectAnswers++;
+          unchecked = false;
+        }
+      }
+      //after iterating through the current choices array with no answers selected, unAnswered will increase.
+      if (unchecked) {
+        unAnswered++;
+      }
+    }
+
+    //jQuery
+    $("#correctAnswer").text("Correct Answers: " + correctAnswers);
+    $("#incorrectAnswer").text("Incorrect Answers: " + incorrectAnswers);
+    $("#unAnswered").text("Unanswered Questions: " + unAnswered);
+    clearInterval(intervalId);
     $("#main").hide();
     $("#results").show();
-    clockRunning = false;
   }
 
-  if (clockRunning) {
-  }
-
-  function run() {
-    clearInterval(intervalID);
-    intervalID = setInterval(decrement, 1000);
-    $("#timer").text("Time Remaining: " + time);
+  function decrement() {
+    if (time === 0) {
+      score();
+    } else {
+      time--;
+      $("#timer").text("Time Remaining: " + time);
+    }
   }
   //count down function
 
